ca32ccc736174132defe04c2b4e79d7a425e22c4
Samitola <samitola986@gmail.com>
2022-01-08 20:25:15 -0500

    Fixed the Current temperature

--- src/app.js
+++ src/app.js
@@ -1,1 +1,19 @@
-alert("hello world");
+function displayTemperature(response) {
+  console.log(response.data);
+
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let maximumElement = document.querySelector("#maximum");
+  let windElement = document.querySelector("#wind");
+
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].main;
+  maximumElement.innerHTML = Math.round(response.data.main.temp_max);
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
+let apiKey = "b1b7ee9d1c74d69e09f208444a66c295";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(displayTemperature);
