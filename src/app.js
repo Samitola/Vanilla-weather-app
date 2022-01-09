c580be9b79d171428caa13d858a3903a74d1f9e7
Samitola <samitola986@gmail.com>
2022-01-09 17:24:57 -0500

    Added the search engine and updated html

--- src/app.js
+++ src/app.js
@@ -42,7 +42,19 @@ function displayTemperature(response) {
   minimumElement.innerHTML = Math.round(response.data.main.temp_min);
 }
 
-let apiKey = "b1b7ee9d1c74d69e09f208444a66c295";
-let city = "New York";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-axios.get(apiUrl).then(displayTemperature);
+function search(city) {
+  let apiKey = "b1b7ee9d1c74d69e09f208444a66c295";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+search("New York");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
